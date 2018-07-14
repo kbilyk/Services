90197e72f4b0e3fca5e59e26f2aa832361b37704
Kostyantyn Bilyk <kos.bilyk@gmail.com>
2018-07-14 10:08:25 +0300

    Initial Commit

---
+++ Services/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Services
+//
+//  Created by Kostiantyn Bilyk on 14.07.18.
+//  Copyright © 2018 Kostiantyn Bilyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
