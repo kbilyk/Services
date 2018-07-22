fd8550b7bde341e8e2b6b123b39b525cf27b25b8
Kostiantyn Bilyk <kos.bilyk@gmail.com>
2018-07-22 14:17:44 +0300

    Added Customer View Controller witth textfields and saving with validation logic. Implemented segue handling functionality.

---
+++ Services/Controller/CustomerViewController.swift
@@ -0,0 +1,102 @@
+//
+//  CustomerViewController.swift
+//  Services
+//
+//  Created by Kostiantyn Bilyk on 22.07.18.
+//  Copyright © 2018 Kostiantyn Bilyk. All rights reserved.
+//
+
+import UIKit
+
+class CustomerViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var infoTextField: UITextField!
+
+    // MARK: - Variables
+
+    var customer: Customer?
+
+    // MARK: - Constants
+
+    enum Constants {
+        enum validation {
+            enum alert {
+                static let title: String = "Validation Error!"
+                static let message: String = "Input Name of the Customer, please."
+            }
+            enum action {
+                static let title: String = "OK"
+            }
+        }
+    }
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let customer = customer {
+            nameTextField.text = customer.name
+            infoTextField.text = customer.info
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Custom Functions
+
+    func saveCustomer() -> Bool {
+        // Name Validation
+        if let name = nameTextField.text, name.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).isEmpty {
+            let alert = UIAlertController(title: Constants.validation.alert.title,
+                                          message: Constants.validation.alert.message,
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: Constants.validation.action.title,
+                                          style: .cancel,
+                                          handler: nil))
+            present(alert, animated: true, completion: nil)
+
+            return false
+        }
+
+        // Create Customer Object if needed
+        if customer == nil {
+            customer = Customer()
+        }
+
+        // Save Customer Object
+        if let customer = customer {
+            customer.name = nameTextField.text
+            customer.info = infoTextField.text
+
+            CoreDataManager.sharedInstance.saveContext()
+        }
+
+        return true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: - Actions
+
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+    }
+
+    @IBAction func save(_ sender: UIBarButtonItem) {
+    }
+}
--- Services/Controller/CustomersTableViewController.swift
+++ Services/Controller/CustomersTableViewController.swift
@@ -11,19 +11,24 @@ import CoreData
 
 class CustomersTableViewController: UITableViewController {
 
+    // MARK: - Variables
+
     lazy var fetchedResultsController: NSFetchedResultsController<Customer> = {
         return CoreDataManager.sharedInstance.fetchedResultsController(entityName: NSStringFromClass(Customer.self),
                                                                        keyForSort: #keyPath(Customer.name))
         }() as! NSFetchedResultsController<Customer>
 
+    // MARK: - Constants
+    enum Constants {
+        enum segues {
+            static let customersToCustomer: String = "customersToCustomer"
+        }
+    }
+
+    // MARK: - View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
 
         do {
             try fetchedResultsController.performFetch()
@@ -32,25 +37,24 @@ class CustomersTableViewController: UITableViewController {
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         if let sections = fetchedResultsController.sections {
+
             return sections.count
         } else {
+
             return 0
         }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let sections = fetchedResultsController.sections {
+
             return sections[section].numberOfObjects
         } else {
+
             return 0
         }
     }
@@ -64,49 +68,25 @@ class CustomersTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let customer = fetchedResultsController.object(at: indexPath) as Customer
+
+        performSegue(withIdentifier: Constants.segues.customersToCustomer, sender: customer)
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        if segue.identifier == Constants.segues.customersToCustomer {
+            let controller = segue.destination as! CustomerViewController
 
+            controller.customer = sender as? Customer
+        }
+    }
+
+    // MARK: - Actions
+
+    @IBAction func add(_ sender: UIBarButtonItem) {
+         performSegue(withIdentifier: Constants.segues.customersToCustomer, sender: nil)
+    }
 }
